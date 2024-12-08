cd326c9170401bf6f9784b351ffe48b9c518fdee
riorimba <monhero78@gmail.co>
2024-12-08 20:59:07 +0700

    Book: Index

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::paginate();
+
+        return response()->json($books);
+    }
+}
