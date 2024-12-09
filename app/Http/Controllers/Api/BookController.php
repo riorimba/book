1b30b82aa801e8813df82a634014fa46950ee414
riorimba <monhero78@gmail.co>
2024-12-09 09:01:24 +0700

    Book: Create

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -8,10 +8,29 @@ use App\Models\Book;
 
 class BookController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $books = Book::paginate();
 
         return response()->json($books);
+    }
+
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'cover_image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'name' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'is_published' => ['required', 'boolean'],
+        ]);
+
+        if ($request->hasFile('cover_image')) {
+            // store new image in public folder
+            $fileName = time() . '.' . $request->file('cover_image')->getClientOriginalExtension();
+            $request->file('cover_image')->move(public_path('images'), $fileName);
+            $validatedData['cover_image'] = 'images/' . $fileName;
+        }
+        Book::create($validatedData);
+
+        return response()->json(['success' => 'Book created successfully']);
     }
 }
