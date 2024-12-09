2c9410b32ae5908af3acdbed444e6540abe3a0d2
riorimba <monhero78@gmail.co>
2024-12-09 10:09:48 +0700

    Authentication: Logout

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,4 @@ Route::apiResource('books', BookController::class);
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::get('me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
+Route::Post('logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum'); 
