f60203d0da8fa0810aaebf76348c46d226ac312d
riorimba <monhero78@gmail.co>
2024-12-09 09:19:18 +0700

    Book: Update

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -36,5 +37,30 @@ class BookController extends Controller
 
     public function show(Book $book){
         return response()->json($book);
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $validatedData = $request->validate([
+            'cover_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'name' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'is_published' => ['required', 'boolean'],
+        ]);
+
+        if ($request->hasFile('cover_image')) {
+            // delete old image
+            Storage::delete('images/' . $book->cover_image);
+
+            // store new image in public folder
+            $fileName = time() . '.' . $request->file('cover_image')->getClientOriginalExtension();
+            $validatedData['cover_image'] = $request->file('cover_image')->move(public_path('images'), $fileName);
+            $validatedData['cover_image'] = 'images/' . $fileName;
+        }
+
+        $book->update($validatedData);
+
+        return response()->json(['success' => 'Book updated successfully']);
     }
 }
